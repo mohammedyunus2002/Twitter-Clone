70d6868fa2b26f495192b7a99fa3daf8950df043
Mohammed Yunus Shaikh <mohammedyunusshaikh@Mohammeds-MacBook-Air.local>
2024-02-07 16:02:59 +0530

    Added useEffect for session

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -4,16 +4,21 @@ import Header from "@/components/Header";
 import NotificationsFeed from "@/components/NotificationsFeed";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Notifications = () => {
     const { data: session, status } = useSession();
     const router = useRouter();
 
+    useEffect(() => {
+        if (!session) {
+            router.push('/');
+        }
+    }, [session, router]);
+
     if (!session) {
-        router.push('/');
-
+        // Don't render anything on the server-side
         return null;
-
     }
 
     return ( 
